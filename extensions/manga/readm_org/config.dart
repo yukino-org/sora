b927a2a3f3f178cd421018a715021a75636ae183
zyrouge <naveencnaveen96@gmail.com>
2022-02-08 20:50:15 +0530

    chore: update deps

--- extensions/manga/readm_org/config.dart
+++ extensions/manga/readm_org/config.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 import 'package:extensions/metadata.dart';
 import 'package:extensions_dev_tools/tools.dart';
 import 'package:path/path.dart' as path;
-import '../../constants.dart';
 
 final String _dirname =
     path.join(Directory.current.path, 'extensions/manga/readm_org');
@@ -10,7 +9,6 @@ final String _dirname =
 final EConfig config = EConfig(
   EMetadata(
     name: 'ReadM.org',
-    author: author,
     type: EType.manga,
     source: ELocalFileDS(
       root: path.join(_dirname, 'src'),
