dbef550a4657e468566181dd3425f9e61e16be4f
ZYROUGE <zyrouge@hotmail.com>
2022-06-28 09:04:22 +0530

    chore: remove twist

--- test/files.dart
+++ test/files.dart
@@ -3,7 +3,6 @@ import 'package:tenka_dev_tools/tenka_dev_tools.dart';
 import '../modules/anime/gogoanime_pe/test.dart' as gogoanime_pe;
 import '../modules/anime/hanime_tv/test.dart' as hanime_tv;
 import '../modules/anime/tenshi_moe/test.dart' as tenshi_moe;
-import '../modules/anime/twist_moe/test.dart' as twist_moe;
 import '../modules/anime/zoro_to/test.dart' as zoro_to;
 import '../modules/manga/fanfox_net/test.dart' as fanfox_net;
 import '../modules/manga/mangadex_org/test.dart' as mangadex_org;
@@ -15,7 +14,6 @@ abstract class TestFiles {
     gogoanime_pe.source: gogoanime_pe.mocked,
     hanime_tv.source: hanime_tv.mocked,
     tenshi_moe.source: tenshi_moe.mocked,
-    twist_moe.source: twist_moe.mocked,
     zoro_to.source: zoro_to.mocked,
   };
 
