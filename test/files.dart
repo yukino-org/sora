9c8b746c27e8de86d4a5fd10a8daa1578e40f28c
zyrouge <zyrouge@hotmail.com>
2023-10-16 16:37:27 +0530

    chore: remove marin.moe & add animepahe.com

--- test/files.dart
+++ test/files.dart
@@ -1,8 +1,8 @@
 import 'package:tenka/tenka.dart';
 import 'package:tenka_dev_tools/tenka_dev_tools.dart';
+import '../modules/anime/animepahe_com/test.dart' as animepahe_com;
 import '../modules/anime/gogoanime_bid/test.dart' as gogoanime_bid;
 import '../modules/anime/hanime_tv/test.dart' as hanime_tv;
-import '../modules/anime/marin_moe/test.dart' as marin_moe;
 import '../modules/manga/fanfox_net/test.dart' as fanfox_net;
 import '../modules/manga/mangadex_org/test.dart' as mangadex_org;
 import '../modules/manga/readm_org/test.dart' as readm_org;
@@ -10,9 +10,9 @@ import '../modules/manga/readm_org/test.dart' as readm_org;
 abstract class TestFiles {
   static final Map<TenkaLocalFileDS, MockedAnimeExtractor> anime =
       <TenkaLocalFileDS, MockedAnimeExtractor>{
+    animepahe_com.source: gogoanime_bid.mocked,
     gogoanime_bid.source: gogoanime_bid.mocked,
     hanime_tv.source: hanime_tv.mocked,
-    marin_moe.source: marin_moe.mocked,
   };
 
   static final Map<TenkaLocalFileDS, MockedMangaExtractor> manga =
