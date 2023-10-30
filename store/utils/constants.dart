ad51bde48ac3ccf9ead4a170163825e7b39fb0cb
zyrouge <zyrouge@hotmail.com>
2023-10-30 20:23:09 +0530

    refactor: fix store base url

--- store/utils/constants.dart
+++ store/utils/constants.dart
@@ -2,7 +2,7 @@ abstract class Constants {
   static const String ghUserName = 'yukino-org';
   static const String ghRepoName = 'sora';
   static const String ghMainBranch = 'main';
-  static const String ghDistBranch = 'main';
+  static const String ghDistBranch = 'dist';
 
   static const String ghMainBranchURL =
       'https://github.com/$ghUserName/$ghRepoName/tree/$ghMainBranch';
