165e7508243d3c35ab1dc7cb8165d26fed469e6c
zyrouge <zyrouge@hotmail.com>
2023-10-30 20:18:20 +0530

    refactor: fix storebase url

--- store/utils/constants.dart
+++ store/utils/constants.dart
@@ -2,10 +2,11 @@ abstract class Constants {
   static const String ghUserName = 'yukino-org';
   static const String ghRepoName = 'sora';
   static const String ghMainBranch = 'main';
+  static const String ghDistBranch = 'main';
 
   static const String ghMainBranchURL =
       'https://github.com/$ghUserName/$ghRepoName/tree/$ghMainBranch';
 
   static const String ghDistBranchRawURL =
-      'https://raw.githubusercontent.com/$ghUserName/$ghRepoName/$ghMainBranch';
+      'https://raw.githubusercontent.com/$ghUserName/$ghRepoName/$ghDistBranch';
 }
